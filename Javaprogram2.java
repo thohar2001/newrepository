ef37d37aa58fdf8a359cf541a3020846477531b3
martenhernebring <marten.hernebring@gmail.com>
2020-10-14 10:10:50 +0200

    la till Mårten

--- Javaprogram2.java
+++ Javaprogram2.java
@@ -1,5 +1,6 @@
 public class Javaprogram2 {
         public static void main(String[] args) {
-            System.out.println("Hej, jag heter Leif"); 
+            System.out.println("Hej, jag heter Leif");
+	    System.out.println("Hej, jag heter Mårten");
         }
 }
