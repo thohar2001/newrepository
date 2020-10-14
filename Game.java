910caaf0a0a57ab4b6a2e40ef881b0dbb9c643f2
Thomas <thohar2001@skola.goteborg.se>
2020-10-14 10:34:04 +0200

    add score

--- Game.java
+++ Game.java
@@ -1,16 +1,26 @@
 import java.io.Console;
 
 public class Game {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
+	int total = 0;
+	int correct = 0;
+	
+
         System.out.println("Welcome to the Countries game.");
 	Console console = System.console();
 	String answer = console.readLine("Which continent does Sweden belong to? ");
-	String continent= "Asia";
+        String continent = "Europe";
+
+        while(total < 10) {
+            
+            total++;
+
 	if (answer.equals(continent)) {
     		System.out.println("Correct!");
+		correct++;
 	} else {
    		System.out.println("No, the answer is " + continent);
         }
-
+      }
     }
 }
